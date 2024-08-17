afc8885a1fedc1683a5000a06b8a6580dd95a3e3
iruizz <isaacvert7@gmail.com>
2024-08-17 15:21:36 -0500

    Added icons to tab bar

--- src/BottomTabs.tsx
+++ src/BottomTabs.tsx
@@ -1,4 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Icon from 'react-native-vector-icons/Ionicons';
+
 import Home from './Home';
 import Profile from './Profile';
 import Settings from './Settings';
@@ -7,7 +9,28 @@ const Tab = createBottomTabNavigator();
 
 function BottomTabs() {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+                let iconName: string;
+    
+                switch (route.name) {
+                case 'Home':
+                    iconName = 'home';
+                    break;
+                case 'Profile':
+                    iconName = 'person-circle';
+                    break;
+                case 'Settings':
+                    iconName = 'settings';
+                    break;
+                default:
+                    iconName = '';
+                }
+                return <Icon name={iconName} size={size} color={color} />;
+            },
+            })}
+        >
             <Tab.Screen name="Home" component={Home} />
             <Tab.Screen name="Profile" component={Profile} />
             <Tab.Screen name="Settings" component={Settings} />
